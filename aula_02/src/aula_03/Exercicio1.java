46dbea780da253a3e78b86f0ddec1edb5545615d
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-04-15 14:50:21 -0300

    :recycle: Refatoração Exercício 1 - Aula 3

--- aula_02/src/aula_03/Exercicio1.java
+++ aula_02/src/aula_03/Exercicio1.java
@@ -1,5 +1,29 @@
 package aula_03;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
 
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int valores[] = new int[3]; 
+		
+		for (int i = 0; i < valores.length; i++) {
+			System.out.printf("Digite o %d° valor: ", i+1);
+			valores[i] = leia.nextInt();
+		}
+		
+		int soma = valores[0] + valores[1];
+		
+		if(soma > valores[2])
+			System.out.printf("\nA soma de %d + %d é maior que %d", valores[0], valores[1], valores[2]);
+		else if(soma < valores[2])
+			System.out.printf("\nA soma de %d + %d é menor que %d", valores[0], valores[1], valores[2]);
+		else
+			System.out.printf("\nA soma de %d + %d é igual a %d", valores[0], valores[1], valores[2]);
+		
+		leia.close();
+	}
 }
