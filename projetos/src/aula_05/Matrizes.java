ea6d1159958d67087244e0356da58efb42168f53
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-04-17 17:02:38 -0300

    :sparkles: Inclusão classes de estudo Vetor e Matriz - Aula 5

---
+++ projetos/src/aula_05/Matrizes.java
@@ -0,0 +1,45 @@
+package aula_05;
+
+import java.util.Scanner;
+
+public class Matrizes {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		
+		//int matrizInteiros[][] = {{10, 15, 35}, {70, 120, 140}, {50, 100, 150}};
+		Float matrizReal[][] = new Float[3][3];
+		int matrizIrregular[][] = {{10, 15}, {70, 120}, {50, 100}};
+		
+		/*
+		for(int linha = 0; linha < matrizInteiros.length; linha++) {
+			for(int coluna = 0; coluna < matrizInteiros.length; coluna++) {
+				System.out.printf("\nMatrizInteiros[%d][%d] = %d", linha, coluna, matrizInteiros[linha][coluna]);
+			}
+		} 
+
+		for(int linha = 0; linha < matrizReal.length; linha++) {
+			for(int coluna = 0; coluna < matrizReal.length; coluna++) {
+				System.out.print("Digite um número real: ");
+				matrizReal[linha][coluna] = leia.nextFloat();
+			}
+		}*/
+		
+		for(int linha = 0; linha < matrizIrregular.length; linha++) {
+			for(int coluna = 0; coluna < matrizIrregular[linha].length; coluna++) {
+				System.out.printf("\nMatrizInteiros[%d][%d] = %d", linha, coluna, matrizIrregular[linha][coluna]);
+			}
+		}
+		
+		/*
+		for(Float[] vetor : matrizReal) {
+			for(var valor : vetor) {
+				System.out.println("Posição atual: " + valor);
+			}
+		} */
+		
+		leia.close();
+	}
+
+}
